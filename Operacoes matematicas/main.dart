8dc919b39c0a99e5315a9b4d8631018c2ed5921c
Anderson Coimbra Choren <andersonchoren@gmail.com>
2023-01-30 17:02:32 -0300

    Aula 1 - Introdução ao Dart

---
+++ Operacoes matematicas/main.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Serve para imprimir algo na tela
+  //print("Hello World!!!");
+
+  // Criando variáveis
+  var name = 'Anderson';
+  var age = 31;
+  var height = 1.8; // Altura em metros
+  var isActive = true;
+
+  print(name);
+
+  int ra = 182023789;
+  String country = "Brasil";
+  double salary = 1800.98;
+  bool isStudent = true;
+}
