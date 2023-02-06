b5bb602519d4546c35e4165c7c677e64498eac66
Anderson Coimbra Choren <andersonchoren@gmail.com>
2023-02-06 17:04:17 -0300

    Operadores matemáticos - Revisão

---
+++ Operacoes matematicas/mecanica.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Entrada
+  int massa = 20; // KG
+  double gravidade = 9.81; // Gravidade
+
+  // Processamento
+  double forcaPeso = massa * gravidade;
+
+  // Saída
+  print("F = $forcaPeso");
+}
